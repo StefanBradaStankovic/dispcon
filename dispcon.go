592d33640ef9ca117056a42739346cff81d9951a
Stefan Stankovic <stefanstankovic@pop-os.localdomain>
2022-04-04 15:49:49 +0200

    Complete code refactor

--- dispcon.go
+++ dispcon.go
@@ -2,7 +2,6 @@ package dispcon
 
 import (
 	"log"
-	"time"
 
 	"periph.io/x/conn/v3/gpio"
 	"periph.io/x/conn/v3/gpio/gpioreg"
@@ -10,274 +9,60 @@ import (
 )
 
 //
-// Version v1.2.0
+//  Version v2.0.0
 //
 //
-// To propperly define sellectedDigits[][]bool values, it is necessary to map out your display
-// according to its pin numbers - eg. 1-10 for single digit screen or 1-12 for quad digit screen with sellector pins
+
+//  Changes state of a LED diode to:
 //
-// Draw an ordinal number from 0 to 9
-func DisplayDrawNumber(sellectedPins []string, sellectedDigits [][]bool, number int) {
-
-	PinsResetLow(sellectedPins[:])
-
-	if number >= 0 && number <= 15 {
-		DiodeOnClusterSellect(sellectedPins, sellectedDigits[number])
-	} else {
-		log.Println("Invalid number")
+//	    0 - Input
+//      1 - Low
+//      2 - High
+//
+func diodeSetState(diodePinName string, diodeState int) {
+	// Load all periph drivers
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
 	}
 
-}
-
-// Flash every segment to visually confirm they all work
-func DisplayCheckSegments(sellectedPins []string) {
-
-	for i := 0; i < len(sellectedPins); i++ {
-		DiodeFlash(sellectedPins[i], 100, 400)
+	// Registers GPIO pin acording to its Name
+	diodePin := gpioreg.ByName(diodePinName)
+	if diodePin == nil {
+		log.Fatal("Failed to open GPIO pin.")
 	}
 
-}
-
-// Draw a running circle animation
-func DisplayCircleRound(sellectedPins []string, speed int) {
-
-	for i := 0; i < len(sellectedPins); i++ {
-		DiodeFlash(sellectedPins[i], time.Duration(speed), 0)
+	if diodeState == 0 {
+		if err := diodePin.In(gpio.PullNoChange, gpio.NoEdge); err != nil {
+			log.Fatal(err)
+		}
 	}
-
-}
-
-// Draw a full circle animation
-func DisplayCircleFlash(sellectedPins []string, speed int) {
-
-	for i := 0; i < len(sellectedPins); i++ {
-		DiodeOn(sellectedPins[i])
+	if diodeState == 1 {
+		if err := diodePin.Out(gpio.Low); err != nil {
+			log.Fatal(err)
+		}
 	}
-
-	time.Sleep(time.Duration(speed) * time.Millisecond)
-
-	for i := 0; i < len(sellectedPins); i++ {
-		DiodeOff(sellectedPins[i])
-	}
-
-	time.Sleep(time.Duration(speed) * time.Millisecond)
-
-}
-
-// Draw a full circle segment by segment
-func DisplayLoading(sellectedPins []string, speed int) {
-
-	for i := 0; i < len(sellectedPins); i++ {
-		DiodeOn(sellectedPins[i])
-		time.Sleep(time.Duration(speed) * time.Millisecond)
-	}
-
-}
-
-// Draw a startup animation
-func DisplayStartupAnimation(sellectedPins []string) {
-
-	DisplayLoading(sellectedPins, 150)
-	DisplayCircleFlash(sellectedPins, 250)
-	DisplayCircleFlash(sellectedPins, 250)
-	DisplayCircleFlash(sellectedPins, 1500)
-}
-
-// Draw four different digits on a quad digit 8-segment display
-func DisplayDrawNumberMultiple(sellectedPins []string, sellectorPins []string, sellectedDigits [][]bool, digits *[]int, mainDone *int) {
-
-	PinsResetLow(sellectedPins[:])
-	PinsResetIn(sellectorPins[:])
-
-	for *mainDone != 1 {
-
-		for i := 0; i < 4; i++ {
-			DiodeOff(sellectorPins[i])
-			DisplayDrawNumber(sellectedPins, sellectedDigits, (*digits)[i])
-			time.Sleep(1 * time.Millisecond)
-			DiodeIn(sellectorPins[i])
+	if diodeState == 2 {
+		if err := diodePin.Out(gpio.High); err != nil {
+			log.Fatal(err)
 		}
 	}
 }
 
-// Flash a LED by setting a pin to HIGH for timeOn amount of milliseconds with a latency of timeOff milliseconds
-func DiodeFlash(inputPin string, timeOn time.Duration, timeOff time.Duration) {
-	// Load all the drivers:
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Open pin for communication
-	pin := gpioreg.ByName(inputPin)
-	if pin == nil {
-		log.Fatal("Failed to open GPIO pin")
-	}
-
-	// Set pin to LOW
-	if err := pin.Out(gpio.Low); err != nil {
-		log.Fatal(err)
-	}
-
-	// fmt.Printf("Flashing pin %s\n", inputPin)
-	time.Sleep(timeOff * time.Millisecond)
-	if err := pin.Out(gpio.High); err != nil {
-		log.Fatal(err)
-	}
-	time.Sleep(timeOn * time.Millisecond)
-	if err := pin.Out(gpio.Low); err != nil {
-		log.Fatal(err)
-	}
-}
-
-// Turn on a number of LEDs permanently by setting the corresponding pins to HIGH
-func DiodeOnCluster(inputPins []string) {
-	// Load all the drivers:
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	for i := 0; i < len(inputPins); i++ {
-		DiodeOn(inputPins[i])
-	}
-}
-
-// Turn off a number of LEDs permanently by setting the corresponding pins to LOW
-func DiodeOffCluster(inputPins []string) {
-	// Load all the drivers:
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	for i := 0; i < len(inputPins); i++ {
-		DiodeOff(inputPins[i])
-	}
-}
-
-// Turn on a number of LEDs permanently by setting the corresponding pins to HIGH
-func DiodeOnClusterSellect(inputPins []string, pinState []bool) {
-	// Load all the drivers:
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	for i := 0; i < len(inputPins); i++ {
-		if pinState[i] {
-			DiodeOn(inputPins[i])
+//  Changes state of an array of LED diodes
+//
+//      - to set the whole array to same state without declaring an array of ints
+//        call the function with []int{} as second argument
+//      - in case you are calling the function with custom diodeStates, defaultState
+//        can be ignored
+//
+func diodeArraySetState(diodePinNames []string, diodeStates []int, defaultState int) {
+	if len(diodeStates) == 0 {
+		for i := 0; i < len(diodePinNames); i++ {
+			diodeStates[i] = defaultState
 		}
 	}
-}
 
-// Turn off a number of LEDs permanently by setting the corresponding pins to LOW
-func DiodeOffClusterSellect(inputPins []string, pinState []bool) {
-	// Load all the drivers:
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
+	for i := 0; i < len(diodePinNames); i++ {
+		diodeSetState(diodePinNames[i], diodeStates[i])
 	}
-
-	for i := 0; i < len(inputPins); i++ {
-		if pinState[i] {
-			DiodeOff(inputPins[i])
-		}
-	}
-}
-
-// Turn on an LED permanently by setting the pin to HIGH
-func DiodeOn(inputPin string) {
-	// Load all the drivers:
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Open pin for communication
-	pin := gpioreg.ByName(inputPin)
-	if pin == nil {
-		log.Fatal("Failed to open GPIO pin")
-	}
-
-	// Set pin to IN
-	if err := pin.In(gpio.PullNoChange, gpio.NoEdge); err != nil {
-		log.Fatal(err)
-	}
-
-	// Set pin to HIGH
-	if err := pin.Out(gpio.High); err != nil {
-		log.Fatal(err)
-	}
-}
-
-// Turn off an LED permanently by setting the pin to LOW
-func DiodeOff(inputPin string) {
-	// Load all the drivers:
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Open pin for communication
-	pin := gpioreg.ByName(inputPin)
-	if pin == nil {
-		log.Fatal("Failed to open GPIO pin")
-	}
-
-	// Set pin to IN
-	if err := pin.In(gpio.PullNoChange, gpio.NoEdge); err != nil {
-		log.Fatal(err)
-	}
-
-	// Set pin to LOW
-	if err := pin.Out(gpio.Low); err != nil {
-		log.Fatal(err)
-	}
-}
-
-// Completelly isolate the diode by setting the pin to IN
-func DiodeIn(inputPin string) {
-	// Load all the drivers:
-	if _, err := host.Init(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Open pin for communication
-	pin := gpioreg.ByName(inputPin)
-	if pin == nil {
-		log.Fatal("Failed to open GPIO pin")
-	}
-
-	// Set pin to IN
-	if err := pin.In(gpio.PullNoChange, gpio.NoEdge); err != nil {
-		log.Fatal(err)
-	}
-}
-
-// RESET ALL PINS BY SETTING THEM TO LOW
-func PinsResetLow(sellectedPins []string) {
-
-	for i := 0; i < len(sellectedPins); i++ {
-		DiodeOff(sellectedPins[i])
-	}
-
-}
-
-// RESET ALL PINS BY SETTING THEM TO IN
-func PinsResetIn(sellectedPins []string) {
-
-	for i := 0; i < len(sellectedPins); i++ {
-		DiodeIn(sellectedPins[i])
-	}
-}
-
-// Split an integer into an array with 4 digits
-func SplitDigits(number int) []int {
-
-	var digits = []int{0, 0, 0, 0}
-	if number >= 0 && number <= 9999 {
-
-		digits[3] = number % 10
-		digits[2] = (number / 10) % 10
-		digits[1] = (number / 100) % 10
-		digits[0] = number / 1000
-	} else {
-		log.Println("Number out of bounds !!!")
-	}
-
-	return digits
 }
---
+++ interface.go
@@ -0,0 +1,84 @@
+package dispcon
+
+import (
+	"log"
+	"time"
+)
+
+//  Set a single pin to INPUT
+func DiodeIn(diodePinName string) {
+	diodeSetState(diodePinName, 0)
+}
+
+//  Set a single pin to LOW
+func DiodeOff(diodePinName string) {
+	diodeSetState(diodePinName, 1)
+}
+
+//  Set a single pin to HIGH
+func DiodeOn(diodePinName string) {
+	diodeSetState(diodePinName, 2)
+}
+
+//  Set an array of pins to INPUT
+func DiodeArrayIn(diodePinNames []string) {
+	diodeArraySetState(diodePinNames, []int{}, 0)
+}
+
+//  Set an array of pins to LOW
+func DiodeArrayOff(diodePinNames []string) {
+	diodeArraySetState(diodePinNames, []int{}, 1)
+}
+
+//  Set an array of pins to HIGH
+func DiodeArrayOn(diodePinNames []string) {
+	diodeArraySetState(diodePinNames, []int{}, 2)
+}
+
+//  Flash a LED diode
+//
+//      - if function is called with second and third argument as 0, it will flash
+//        for half a second with half a second wait time
+//
+func DiodeFlash(diodePinName string, timeOn time.Duration, timeOff time.Duration) {
+	if timeOn == 0 {
+		timeOn = 500
+	}
+	if timeOff == 0 {
+		timeOff = 500
+	}
+
+	DiodeOn(diodePinName)
+	time.Sleep(timeOn * time.Millisecond)
+	DiodeOff(diodePinName)
+	time.Sleep(timeOff * time.Millisecond)
+}
+
+//  Driver for displaying a symbol on a single digit screen
+func DisplaySingleDigit(diodePinNames []string, diodeStates [][]int, number int) {
+
+	DiodeArrayOff(diodePinNames)
+
+	if number >= 0 && number <= 15 {
+		diodeArraySetState(diodePinNames, diodeStates[number], 0)
+	} else {
+		log.Println("Invalid number")
+	}
+
+}
+
+//  Driver for displaying symbols on a multiple digit screen
+func DisplayMultipleDigits(diodePinNames []string, groundPinNames []string, diodeStates [][]int, digits *[]int, mainDone *int) {
+
+	DiodeArrayIn(diodePinNames)
+
+	for *mainDone != 1 {
+
+		for i := 0; i < len(*digits); i++ {
+			DiodeOff(groundPinNames[i])
+			DisplaySingleDigit(diodePinNames, diodeStates, (*digits)[i])
+			time.Sleep(1 * time.Millisecond)
+			DiodeIn(groundPinNames[i])
+		}
+	}
+}
